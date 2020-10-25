4584ab18b7ec65e902955a4b5b91e3d0948f2857
jflorespastrana <73243004+jflorespastrana@users.noreply.github.com>
2020-10-25 11:24:25 +0100

    Create DistinctValues.java

---
+++ DistinctValues.java
@@ -0,0 +1,19 @@
+//2.Write a program DistinctValues.java that takes three integer command-line arguments a, b, and c and print the number of distinct values (1, 2, or 3) among a, b, and c.
+import java.util.Scanner;
+
+public class DistinctValues { 
+    public static void main(String[] args) { 
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if (a==b && a==c) {
+            System.out.println(1);
+        } else if ((a==b && a!=c) || (a==c && a!=b) || (a==b && a!=c)  || (b==c && b!=a)) {
+            System.out.println(2);
+        }
+        else {
+        	System.out.println(3);	
+        }
+}
+}
